7c992e94eefb38b6db9d4f48c858af9961effec2
Razvan Dumitrescu <razvandf@gmail.com>
2019-08-16 09:43:42 +0300

    Initial Commit

---
+++ Cola_machine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cola_machine
+//
+//  Created by Razvan Dumitrescu on 16/08/19.
+//  Copyright © 2019 Razvan Dumitrescu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
